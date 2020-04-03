a45f4c005aeb8510a3750176e1c4b59db1c72e93
Vitoria Chacon <vitoria.chacon@hotmail.com>
2020-04-02 21:57:20 -0300

    Imprimindo jogador e turno

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces()); //imprimir tabuleiro na tela
+				UI.printMatch(chessMatch);//imprimir tabuleiro na tela
 				System.out.println();
 				System.out.print("Source: "); 
 				ChessPosition source = UI.readChessPosition(sc);
